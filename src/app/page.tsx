6bd3638651c317f8a63cb2d43b17ae18e4c1fb65
c0dep4nther <markdcg@outlook.com>
2023-07-16 15:27:04 +0800

    Subreddit Creation Added - backend connection - toast notifcations when throwing errors - 3-21 character limits for subreddit naming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import GeneralFeed from "@/components/GeneralFeed";
 import { buttonVariants } from "@/components/ui/Button";
 import { HomeIcon } from "lucide-react";
 import Link from "next/link";
@@ -9,7 +8,6 @@ export default function Home() {
       <h1 className="font-bold text-3xl md:text-4xl">Your feed</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
         {/* feed */}
-        <GeneralFeed />
 
         {/* subreddit info */}
         <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
@@ -23,8 +21,8 @@ export default function Home() {
           <dl className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
             <div className="flex justify-between gap-x-4 py-3">
               <p className="text-zinc-500">
-                Your personal frontpage. Come here to check in with your
-                favorite communities.
+                Your personal Film Fusion homepage. Come here to check in with
+                your favorite communities.
               </p>
             </div>
 
--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -1,13 +1,56 @@
 "use client";
 
 import { Button } from "@/components/ui/Button";
-import { Input } from "@/components/ui/input";
+import { Input } from "@/components/ui/Input";
+import { useCustomToast } from "@/hooks/use-custom-toast";
+import { toast } from "@/hooks/use-toast";
+import { CreateSubredditPayload } from "@/lib/validators/subreddit";
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
-function Page() {
-  const router = useRouter();
+type Props = {};
+
+function Page({}: Props) {
   const [input, setInput] = useState<string>("");
+  const router = useRouter();
+  const { loginToast } = useCustomToast();
+
+  const { mutate: createcommunity, isLoading } = useMutation({
+    mutationFn: async () => {
+      const payload: CreateSubredditPayload = {
+        name: input,
+      };
+
+      const { data } = await axios.post("/api/subreddit", payload);
+      return data as string;
+    },
+    onError: (err) => {
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 409) {
+          return toast({
+            title: "This subreddit already exists.",
+            description: "Please choose a different subreddit name.",
+            variant: "destructive",
+          });
+        }
+
+        if (err.response?.status === 422) {
+          return toast({
+            title: "Invalid subreddit name.",
+            description: "Please choose a name between 3 and 21 characters.",
+            variant: "destructive",
+          });
+        }
+
+        if (err.response?.status === 401) {
+          return loginToast();
+        }
+      }
+    },
+  });
 
   return (
     <div>
@@ -18,13 +61,14 @@ function Page() {
             <h1 className="text-xl font-semibold">Create a Community</h1>
           </div>
 
-          <hr className="bg-red-500 h-px" />
+          <hr className="bg-zinc-500 h-px" />
 
           <div>
             <p className="text-lg font-medium">Name</p>
             <p className="text-xs pb-2">
               Community names including capitalization cannot be changed.
             </p>
+
             <div className="relative">
               <p className="absolute text-sm left-0 w-8 inset-y-0 grid place-items-center text-zinc-400">
                 r/
@@ -41,7 +85,20 @@ function Page() {
             <Button variant="ghost" onClick={() => router.back()}>
               Cancel
             </Button>
-            <Button disabled={input.length === 0}>Create Community</Button>
+
+            {isLoading ? (
+              <Button disabled>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Creating Community
+              </Button>
+            ) : (
+              <Button
+                disabled={input.length === 0}
+                onClick={() => createcommunity()}
+              >
+                Create Community
+              </Button>
+            )}
           </div>
         </div>
       </div>

2d776d084df80dbf44d6c34007a91037faab3135
itscheriespace <trisha_sayo@dlsu.edu.ph>
2023-07-24 14:58:00 +0800

    feat: add edit comment form with optimistic updates
    Instead of rendering the comment.text directly from props, we store
    the value in state and manage the updates ourselves. We need this
    to do this to enable optimistic updates.
    
    Optimistic updates mitigates an issue where calling router.refresh()
    in children components does not trigger React Suspense.

--- src/lib/validators/comment.ts
+++ src/lib/validators/comment.ts
@@ -7,3 +7,12 @@ export const commentValidator = z.object({
 });
 
 export type CommentRequest = z.infer<typeof commentValidator>;
+
+export const updateCommentValidator = z.object({
+  id: z.string(),
+  postId: z.string(),
+  text: z.string(),
+  replyToId: z.string().nullable(),
+});
+
+export type UpdateCommentRequest = z.infer<typeof updateCommentValidator>;

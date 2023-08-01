ec6b7bfbd7aff9b363344f4910ed5dd417ff3c28
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2023-08-01 10:08:34 +0800

    Edited user profile tab to fetch according to user

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -2,11 +2,13 @@ import { redirect } from "next/navigation";
 import React from 'react'
 import Link from "next/link";
 
-import { User } from "next-auth";
+import { db } from "@/lib/db";
 import UserFeed from "@/components/UserFeed";
+import GeneralFeed from "@/components/GeneralFeed";
 import UserAvatar from "@/components/UserAvatar";
 import { buttonVariants } from "@/components/ui/Button";
-import { authOptions, getAuthSession } from "@/lib/auth";
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
+
 
 export const metadata = {
     title: "User Profile",
@@ -14,18 +16,41 @@ export const metadata = {
 };
 
 type Props = {
-    user: Pick<User, "name" | "email" | "image">;
+    params: {
+        username: string;
+    };
   };
 
-async function page({ user }: Props) {
-    const session = await getAuthSession();
+async function page({ params }: Props) {
+    const { username } = params;
+    
+    const user_name = await db.user.findFirst({
+        where: {
+          name: username,
+        },
+        include: {
+          Post: {
+            include: {
+              author: true,
+              votes: true,
+              comments: true,
+              subreddit: true,
+            },
+            orderBy: {
+              createdAt: "desc",
+            }, // added to prevent duplicate posts
+    
+            take: INFINITE_SCROLLING_PAGINATION_RESULTS,
+          },
+        },
+      });
 
       return (
       <div>
         <h1 className="font-bold text-3xl md:text-4xl">Your profile</h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
             {/* feed */}
-            {<UserFeed />}
+            <UserFeed />
 
             {/* Profile bar */}
             <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
@@ -43,15 +68,15 @@ async function page({ user }: Props) {
             <dl className="-my-3 divide-y divide-gray-300 px-6 py-4 text-sm leading-6">
                 <div className="justify-between gap-x-4 py-3">
                     <p className="font-semibold text-2xl text-center items-center gap-0.5">
-                        {session?.user.name}
+                        {user_name?.username}
                     </p>
                     <p className="font-semibold text-lg text-center items-center gap-1.5">
-                        u/{session?.user.username}
+                        u/{user_name?.username}
                     </p>
                 </div>
                 <div className="justify-between gap-x-4 py-3">
                     <p className="text-zinc-500 text-center items-center">
-                        {/*TODO: {session?.user.about}*/}
+                        {user_name?.about}
                     </p>
                 </div>
 

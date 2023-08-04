8f8963961a82712ae7fb4234ef1ce646fccffa6e
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2023-08-04 08:50:25 +0800

    Added avatar routing

---
+++ src/app/api/user/avatar/route.ts
@@ -0,0 +1,33 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth/next";
+import { db } from "@/lib/db";
+import { authOptions } from "@/lib/auth";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "PUT") {
+    // fetch all posts
+    const session = await getServerSession(req, res, authOptions);
+    if (!session) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+	const { image } = req.body;
+    try {
+      const result = await db.user.update({
+        where: {
+          email: session?.user?.email || "",
+        },
+        data: {
+          image
+        },
+      });
+      return res.status(200).json(result);
+    } catch (error: any) {
+      return res.status(403).json({ error: error.message });
+    }
+  }
+}

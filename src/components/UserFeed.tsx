83d348b72fdfdb2d2dc8139eb27531737053d2b9
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2023-08-06 22:46:54 +0800

    Added tabs for user profile

--- src/components/UserFeed.tsx
+++ src/components/UserFeed.tsx
@@ -2,6 +2,8 @@ import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
 import { db } from "@/lib/db";
 import React from "react";
 import PostFeed from "./PostFeed";
+import { PenLineIcon, MessageSquareIcon } from "lucide-react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/Tabs"
 
 type Props = {
   username: string | null | undefined;
@@ -30,7 +32,30 @@ async function UserFeed({ username }: Props) {
     take: INFINITE_SCROLLING_PAGINATION_RESULTS,
   });
 
-  return <PostFeed initialPosts={posts} authorId={user?.id} />;
+  return (
+    <>
+      <Tabs defaultValue="posts" className="col-span-2">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="posts">
+            <PenLineIcon className="w-4 h-4 mr-2"/>
+            Posts
+          </TabsTrigger>
+          <TabsTrigger value="comments">
+            <MessageSquareIcon className="w-4 h-4 mr-2"/>
+            Comments
+          </TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="posts">
+          <PostFeed initialPosts={posts} authorId={user?.id} />
+        </TabsContent>
+        
+        <TabsContent value="comments">
+
+        </TabsContent>
+      </Tabs>
+    </>
+  )
 }
 
 export default UserFeed;

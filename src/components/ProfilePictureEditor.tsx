0aa5db5a1d60ee542e0a255836e19dbbef89758f
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2023-08-08 15:55:18 +0800

    Placed temporary code for upload profile

--- src/components/ProfilePictureEditor.tsx
+++ src/components/ProfilePictureEditor.tsx
@@ -9,57 +9,18 @@ import {
     CardHeader,
     CardTitle,
   } from "./ui/Card";
-import { Label } from "./ui/Label";
 import { Button } from "./ui/Button";
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 import { toast } from "@/hooks/use-toast";
 import Image from "next/image";
-import useAvatarStore from "@/app/store/avatarStore";
-import { Router } from "lucide-react";
 import { useRouter } from "next/navigation";
-
 
 type Props = {
   user: Pick<User, "id" | "image">;
 };
 
 function ProfilePictureEditor({ user }: Props) {
-    const isBrowser = typeof window !== "undefined";
-    
-    const { avatar, setAvatar } = useAvatarStore((state) => ({
-        avatar: state.avatar,
-        setAvatar: state.setAvatar,
-    }));
-    
-    const cloudinaryWidget = isBrowser &&
-    (window as any).cloudinary?.createUploadWidget(
-      {
-        cloudName: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUDNAME,
-        uploadPreset: process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET,
-        sources: ["local", "facebook", "instagram", "camera", "url"],
-        multiple: false,
-        cropping: true,
-        showAdvancedOptions: false,
-        croppingAspectRatio: 1,
-        folder: "demo_uw_folder",
-      },
-      (error: unknown, result: any) => {
-        if (!error && result && result.event === "success") {
-          const image = result.info.secure_url;
-          axios
-            .put("/api/users/avatar", { image })
-            .then(function (response) {
-              console.log(response.data);
-              setAvatar(response.data.image);
-            })
-            .catch(function (error) {
-              console.log(error);
-            });
-        }
-        if (error) console.log("widget error", error);
-      }
-    );
 
 const router = useRouter();
 
@@ -102,7 +63,7 @@ return (
             <div className="">
                 <Image
                 alt="..."
-                src={avatar || (user.image as string)}
+                src={user.image as string}
                 className="shadow-xl rounded-full h-auto align-middle border ml-3 max-w-150-px"
                 width={100}
                 height={100}
@@ -114,8 +75,7 @@ return (
           
         <CardFooter>
             <Button
-                id="upload_widget"
-                onClick={() => cloudinaryWidget.open()}>
+            >
                 Change Photo
             </Button>
         </CardFooter>
